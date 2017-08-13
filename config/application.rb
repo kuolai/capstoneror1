f870aa1b2b4639203cc1c28afb5918735301897e
kuolai <kuolaisha@gmail.com>
2017-08-13 01:00:30 -0700

    README.md https CORS added

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,15 @@ module Capstoneror1
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    config.middleware.insert_before 0, "rack::Cors" do
+        allow do
+            origins '*' #'github.com'
+        resource '/api/*',
+            :headers => :any,
+            :methods => [:get] #[:get, :post, :put, :delete, :options]
+        end
+    end
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
